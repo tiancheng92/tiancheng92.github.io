78e68f378f207b946c6d18d34b52d47cb693c903
徐天成 <tiancheng.xu@yo-star.com>
2022-06-20 17:04:09 +0800

    feat(blog): update config

--- _includes/pageview-providers/leancloud/leancloud.js
+++ _includes/pageview-providers/leancloud/leancloud.js
@@ -13,7 +13,7 @@
     appKey = options.appKey;
     appClass = options.appClass;
     AV.init({
-      serverURLs: 'https://avoscloud.com',
+      serverURLs: 'https://soidl7vx.lc-cn-n1-shared.com',
       appId: appId,
       appKey: appKey
     });
